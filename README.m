251485c2325483c0e328b9a509badce1c428ca40
KOTESHWARRR <mrkoteshwargowd@gmail.com>
2026-04-24 23:31:38 +0530

    Revise README for Java practice repository
    Updated the README file to improve clarity and structure.

---
+++ README.m
@@ -0,0 +1,21 @@
+# Java Practice 🚀
+
+This repository contains Java programs from basic to intermediate level.
+
+## 📚 Topics Covered
+- Basics (Hello World, Addition, Even/Odd)
+- Number Programs (Palindrome, Prime, Factorial)
+- Arrays
+- Strings
+- DSA (coming soon)
+
+## 📁 Folder Structure
+- basics/
+- arrays/
+- strings/
+
+## 💻 Technologies
+- Java
+
+## 👨‍💻 Author
+Koteshwar
